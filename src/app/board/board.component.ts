47cb7348099ffe1ec983d04413f5fe6a5f43b04f
Kunoros <patrick.laranjo@gmail.com>
2020-10-01 18:21:07 +0200

    Edit two command lines.

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -12,33 +12,13 @@ export class BoardComponent implements OnInit {
 
   board;
 
-  // board = {
-  //   lists: [
-  //     {
-  //       title: 'list 1',
-  //       cards: [{ title: 'card 1' }, { title: 'card 2' }],
-  //     },
-  //     {
-  //       title: 'list 2',
-  //       cards: [{ title: 'card 3' }, { title: 'card 4' }],
-  //     },
-  //     {
-  //       title: 'list 2',
-  //       cards: [{ title: 'card 3' }, { title: 'card 4' }],
-  //     },
-  //     {
-  //       title: 'list 2',
-  //       cards: [{ title: 'card 3' }, { title: 'card 4' }],
-  //     },
-  //   ],
-  // };
-
   constructor(
     private _activatedRoute: ActivatedRoute,
     private _board: BoradService
   ) {}
 
   ngOnInit(): void {
+    // Get detailed board information using id from board service
     this._activatedRoute.paramMap.subscribe((params) => {
       this.id = params.get('id');
       this.board = this._board.getBoardDetails(this.id);
--- src/app/manage-boards/manage-boards.component.ts
+++ src/app/manage-boards/manage-boards.component.ts
@@ -9,13 +9,6 @@ import { BoradService } from '../board/borad.service';
 })
 export class ManageBoardsComponent implements OnInit {
   boards;
-  // boards = [
-  //   { id: '1', name: 'Board 1' },
-  //   { id: '2', name: 'Board 2' },
-  //   { id: '3', name: 'Board 3' },
-  //   { id: '4', name: 'Board 4' },
-  //   { id: '5', name: 'Board 5' },
-  // ];
 
   constructor(private _route: Router, private _board: BoradService) {}
 
@@ -23,6 +16,7 @@ export class ManageBoardsComponent implements OnInit {
     this._route.navigate(['manage-board', board]);
   }
 
+  // Get induvial boards from the board service
   ngOnInit(): void {
     this.boards = this._board.getBoards();
   }
